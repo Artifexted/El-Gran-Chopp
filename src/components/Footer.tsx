109d1e3e8311e426ad959cee03a59fd2afeb5058
Artifexted <artifexted@gmail.com>
2025-01-17 18:31:10 -0300

    Correccion de posicionamiento de textos en footer large

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Footer() {
 	return (
 		<footer className="py-2 flex flex-col mx-4 md:flex-row md:justify-between cursor-default">
-			<p className="my-2 text-center max-lg:mx-auto lg:w-[25%] lg:justify-start">
+			<p className="my-2 text-center max-lg:mx-auto lg:w-[25%] lg:text-left">
 				Copyright © <span className="text-destacable">El Gran Chopp</span> {new Date().getFullYear()}. 
                 Todos los derechos reservados.
 			</p>
@@ -17,7 +17,7 @@ export default function Footer() {
 					<FbIcon width={32} height={32} />
 				</Link>
 			</div>
-			<p className="my-2 text-center max-lg:mx-auto lg:w-[25%] lg:justify-end">
+			<p className="my-2 text-center max-lg:mx-auto lg:w-[25%] lg:text-right">
 				Sitio web hecho por 
 				<span className="ml-1">
 					<Link
